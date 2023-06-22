8ef7899e534886b3abaa0231e6fab8b0132876df
Kornel <102041440+KornelPudlo@users.noreply.github.com>
2023-06-22 14:47:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('Raw Data Table with Filters Selection')
+st.write('This is my very own project to create a data frame with a filters selection')
